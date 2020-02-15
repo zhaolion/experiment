5aa3f1e811cbaeee2b2ee46d39902d1d3514b273
lion.zhao <lion.zhao@liulishuo.com>
2020-02-15 20:25:53 +0800

    using gonconvey for test

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/zhaolion/experiment
 
 go 1.13
 
-require github.com/stretchr/testify v1.4.0
+require (
+	github.com/smartystreets/goconvey v1.6.4
+	github.com/stretchr/testify v1.4.0
+	gopkg.in/yaml.v2 v2.2.8 // indirect
+)
--- structure/circle_queue_test.go
+++ structure/circle_queue_test.go
@@ -3,12 +3,13 @@ package structure
 import (
 	"testing"
 
+	. "github.com/smartystreets/goconvey/convey"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestCirCleQueue(t *testing.T) {
 	size := 3
-	queue := NewCirCleQueue(3)
+	queue := NewCirCleQueue(size)
 	assert.True(t, queue.IsEmpty())
 	assert.Equal(t, -1, queue.Front())
 	assert.Equal(t, -1, queue.Rear())
@@ -27,7 +28,7 @@ func TestCirCleQueue(t *testing.T) {
 	assert.Equal(t, -1, e)
 	assert.False(t, ok)
 
-	queue = NewCirCleQueue(3)
+	queue = NewCirCleQueue(size)
 	queue.Enqueue(1)
 	queue.Enqueue(2)
 	queue.Dequeue()
@@ -39,4 +40,50 @@ func TestCirCleQueue(t *testing.T) {
 	queue.Enqueue(4)
 	assert.Equal(t, 4, queue.Front())
 	assert.Equal(t, 4, queue.Rear())
+}
+
+func TestCirCleQueue_Goconvey(t *testing.T) {
+	Convey("CirCleQueue usages", t, func() {
+		size := 3
+
+		Convey("init a queue by default", func() {
+			queue := NewCirCleQueue(size)
+			So(queue.IsEmpty(), ShouldBeTrue)
+			So(queue.Front(), ShouldEqual, -1)
+			So(queue.Rear(), ShouldEqual, -1)
+		})
+
+		Convey("enqueue & dequeue many times", func() {
+			queue := NewCirCleQueue(size)
+			for i := 0; i < size; i++ {
+				So(queue.Enqueue(1), ShouldBeTrue)
+			}
+			So(queue.Enqueue(1), ShouldBeFalse)
+
+			for i := 0; i < size; i++ {
+				e, ok := queue.Dequeue()
+				So(e, ShouldNotEqual, -1)
+				So(ok, ShouldBeTrue)
+			}
+			e, ok := queue.Dequeue()
+			So(e, ShouldEqual, -1)
+			So(ok, ShouldBeFalse)
+		})
+
+		Convey("enqueue and dequeue", func() {
+			queue := NewCirCleQueue(size)
+			queue.Enqueue(1)
+			queue.Enqueue(2)
+			queue.Dequeue()
+			queue.Enqueue(3)
+			So(queue.Front(), ShouldEqual, 2)
+			So(queue.Rear(), ShouldEqual, 3)
+			queue.Dequeue()
+			queue.Dequeue()
+			queue.Enqueue(4)
+			So(queue.Front(), ShouldEqual, 4)
+			So(queue.Rear(), ShouldEqual, 4)
+			So(queue.IsEmpty(), ShouldBeFalse)
+		})
+	})
 }
